df539498e1ea5a8856b3f897c33fc33e0c0dbe62
Rob Martin <robemartin04@hotmail.com>
2025-09-21 22:23:24 -0500

    Fix formatting of form in index.php

--- index.php
+++ index.php
@@ -9,12 +9,11 @@
         <title>Name</title>
     </head>
     <body>
+        <h2>Please enter your first and last name:</h2>
         <form action="courses.php" method="POST">
         First name: <input type="text" name="fname"><br><br>
         Last name:  <input type="text" name="lname"><br><br>
         <input type="submit">
-        </form>
-        
-        
+        </form>       
     </body>
 </html>
